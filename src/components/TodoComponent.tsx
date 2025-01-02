66cc2756a05e37fbabbbd40f9071f6354244fb09
Вадим <pvs.versia@gmail.com>
2025-01-03 00:35:03 +0200

    amended

--- src/components/TodoComponent.tsx
+++ src/components/TodoComponent.tsx
@@ -14,10 +14,10 @@ type IProps = {
 
 export const TodoComponent: FC<IProps> = ({ item }) => {
   return (
-    <Card className={"w-[200px] bg-gradient-to-r from-gray-100 to-red-300"}>
+    <Card className={"w-[200px] text-xl font-black"}>
       <CardHeader>
         <CardTitle>{item.id}</CardTitle>
-        <CardDescription>{item.title}</CardDescription>
+        <CardDescription>{item.todo}</CardDescription>
       </CardHeader>
       <CardContent>
         <p className={"text-black-900"}>{item.completed}</p>
