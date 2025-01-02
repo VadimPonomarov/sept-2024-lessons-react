573f7d1f9a617aa1c81e23fa43be3b1fab04fb6a
Вадим <pvs.versia@gmail.com>
2025-01-02 21:05:08 +0200

    hw2

---
+++ src/components/TodoComponent.tsx
@@ -0,0 +1,27 @@
+import { ITodo } from "@/services/interfaces";
+import { FC } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card.tsx";
+
+type IProps = {
+  item: ITodo;
+};
+
+export const TodoComponent: FC<IProps> = ({ item }) => {
+  return (
+    <Card className={"w-[200px] bg-gradient-to-r from-blue-100 to-green-300"}>
+      <CardHeader>
+        <CardTitle>{item.id}</CardTitle>
+        <CardDescription>{item.title}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p className={"text-black-900"}>{item.completed}</p>
+      </CardContent>
+    </Card>
+  );
+};
---
+++ src/services/todo.service.ts
@@ -0,0 +1,7 @@
+const baseUrl = import.meta.env.VITE_API_JPH;
+export const todoService = {
+  todos: () =>
+    fetch(baseUrl + "/" + "todos").then((response) => response.json()),
+  todoById: (id: string) =>
+    fetch(baseUrl + "/" + "todos" + id).then((response) => response.json()),
+};
