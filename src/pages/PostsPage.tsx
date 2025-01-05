f0049fe0ee0960ce724f9fcabcbededb09a88d3d
Вадим <pvs.versia@gmail.com>
2025-01-05 16:42:43 +0200

    Fixed positioning

--- src/pages/PostsPage.tsx
+++ src/pages/PostsPage.tsx
@@ -1,29 +1,29 @@
-import { FC, useEffect, useState } from "react";
-import { postsService } from "@/services/postsService.ts";
-import { IPost } from "@/models/posts.interfaces.ts";
-import { PostCard } from "@/components/PostCard.tsx";
+import {FC, useEffect, useState} from "react";
+import {postsService} from "@/services/postsService.ts";
+import {IPost} from "@/models/posts.interfaces.ts";
+import {PostCard} from "@/components/PostCard.tsx";
 
 type IProps = object;
 
 export const PostsPage: FC<IProps> = () => {
-  const [posts, setPosts] = useState<IPost[]>([]);
-  const getUsers = async () => await postsService.posts();
+    const [posts, setPosts] = useState<IPost[]>([]);
+    const getUsers = async () => await postsService.posts();
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const { posts } = await getUsers();
-        setPosts(posts);
-      } catch (e) {
-        console.log(e);
-      }
-    })();
-  }, []);
+    useEffect(() => {
+        (async () => {
+            try {
+                const {posts} = await getUsers();
+                setPosts(posts);
+            } catch (e) {
+                console.log(e);
+            }
+        })();
+    }, []);
 
-  return (
-    <div className={"flex flex-wrap justify-evenly gap-2"}>
-      {!!posts.length &&
-        posts.map((item) => <PostCard key={item.id} item={item} />)}
-    </div>
-  );
+    return (
+        <div className={"mt-[40px] flex flex-wrap justify-evenly gap-2"}>
+            {!!posts.length &&
+                posts.map((item) => <PostCard key={item.id} item={item}/>)}
+        </div>
+    );
 };
