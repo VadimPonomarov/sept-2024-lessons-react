5a0e5b2144f44cae6d54b08246952a4249217d91
Вадим <pvs.versia@gmail.com>
2025-01-11 15:41:39 +0200

    with useFetch

--- src/hooks/use-fetch/interfaces.ts
+++ src/hooks/use-fetch/interfaces.ts
@@ -1,16 +1,17 @@
-import { IUsersResponse as UR, IUsersSearch as US } from "@/interfaces/users.interfaces.ts";
-import { IPostsResponse as PR, IPostsSearch as PS } from "@/interfaces/posts.interfaces.ts";
+import { IUsersSearch as US } from "@/interfaces/users.interfaces.ts";
+import { IPostsSearch as PS } from "@/interfaces/posts.interfaces.ts";
 import React from "react";
 
-type CR = UR | PR;
 type CS = US | PS;
-type SU<T> = (prevState: T | undefined) => T | undefined;
-type T = CR | (UR & SU<PR>) | (SU<UR> & PR) | (SU<UR> & SU<PR>) | undefined;
 
-export interface IProps {
+export interface IProps<T> {
   params?: Partial<CS> | {};
   set: React.Dispatch<React.SetStateAction<T>>;
   cb: (params?: Partial<CS>) => Promise<T>;
 }
+
+
+
+
 
 
