b1d8884d84c4af04db30185957f310a85b03422d
Вадим <pvs.versia@gmail.com>
2025-01-30 22:21:04 +0200

    with apiUsers + axios

--- src/api/apiUsers.ts
+++ src/api/apiUsers.ts
@@ -1,33 +1,37 @@
-import { baseUrl } from "@/common/constants/constants.ts";
 import {
   IUser,
   IUserCartResponse,
   IUsersResponse,
   IUsersSearch,
 } from "@/common/interfaces/users.interfaces.ts";
+import { getAxios } from "@/api/getAxios.ts";
+import { baseUrl } from "@/common/constants/constants.ts";
 
+const api = getAxios(baseUrl + "/users");
 export const apiUsers = {
   users: async (params?: Partial<IUsersSearch>): Promise<IUsersResponse> => {
     try {
       const qParams = new URLSearchParams(params as Record<string, string>);
-      const response = await fetch(`${baseUrl}/users?${qParams.toString()}`);
-      return await response.json();
+      const response = await api.get("/", {
+        params: qParams,
+      });
+      return await response.data;
     } catch (e) {
       console.error(e);
     }
   },
   userById: async (userId: string): Promise<IUser> => {
     try {
-      const response = await fetch(`${baseUrl}/users/${userId}`);
-      return await response.json();
+      const response = await api.get(`/${userId}`);
+      return await response.data;
     } catch (e) {
       console.log(e);
     }
   },
   userByIdCarts: async (userId: string): Promise<IUserCartResponse> => {
     try {
-      const response = await fetch(`${baseUrl}/users/${userId}/carts`);
-      return await response.json();
+      const response = await api.get(`/${userId}/carts`);
+      return await response.data;
     } catch (e) {
       console.log(e);
     }
